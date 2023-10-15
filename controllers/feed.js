06c9ad82328faad164da2a905fb562290167ff03
sk2597 <smruti.karade25@gmail.com>
2023-10-15 18:25:11 +0530

    getPosts

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,29 +2,16 @@ const { validationResult } = require("express-validator");
 const Post = require('../models/post')
 
 exports.getPosts = (req, res, next) => {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res
-    .status(422)
-    .json({
-      message: "Validation Failed, entered data is incorrect",
-      errors: errors.array()
-    });
-  }
-  res.status(200).json({
-    posts: [
-      { 
-        _id: "1",
-        title: 'First Post', 
-        content: 'This is the first post!',
-        imageUrl: "",
-        creator: {
-          name: "Smruti"
-        },
-        createdAt: new Date() 
-      }
-    ]
-  });
+  Post.find()
+  .then(posts => {
+    res.status(200).json({message: 'Fetched posts succesfully', posts: posts});
+  })
+  .catch(err => {
+    if(!err.statusCode){
+      err.statusCode = 500;
+    }
+    next(err);
+  })
 };
 
 exports.createPost = (req, res, next) => {
@@ -69,7 +56,7 @@ exports.getPost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
-      res.statusCode(200).json({
+      res.status(200).json({
         message: 'post fetched',
         post: post
       });
