420618e22b03adca0aacfbfcf5356206f4b2944a
sk2597 <smruti.karade25@gmail.com>
2023-10-25 00:11:21 +0530

    adding user details validations

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({message: message});
+    const data = errorrror.data;
+    res.status(status).json({message: message, data: data});
 })
 
 mongoose.connect(uri)
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 
-router.put('/signup');
+const User = require('../models/user');
+const authController = require('../controllers/auth');
+
+router.put('/signup', [
+  body('email').isEmail()
+  .withMessage('Please enter a valid email')
+  .custom((value, { req }) => {
+    return User.findOne({email: value}).then(userDoc => {
+      if (userDoc){
+        return Promise.reject("Email Address Already Exists!");
+      }
+    })
+  })
+  .normalizeEmail(),
+  body('password').trim().isLength({min: 5}),
+  body('name').trim().not().isEmpty()
+],
+  authController.signup
+);
 
 module.exports = router;
