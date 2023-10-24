6c80510702caf1c7afc6183cd3f43194b68d89da
sk2597 <smruti.karade25@gmail.com>
2023-10-24 23:54:41 +0530

    setting up authentication routes and controllers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -39,6 +40,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
